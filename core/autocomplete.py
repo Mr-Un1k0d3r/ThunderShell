6b82550ff2d834a4d31cc9710b74f8eb198f8ccd
tazz0 <33385219+tazz0@users.noreply.github.com>
2017-12-14 21:16:53 -0500

    Upload autocomplete.py

---
+++ core/autocomplete.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+class Completer(object):
+    
+    def __init__(self, options):
+        self.options = sorted(options)
+
+    def complete(self, text, state):
+        if state == 0:  # on first trigger, build possible matches
+            if text:  # cache matches (entries that start with entered text)
+                self.matches = [s for s in self.options 
+                                    #if text in s]
+                                    if s and s.startswith(text)]
+            else:  # no text entered, all matches possible
+                self.matches = self.options[:]
+
+        # return match indexed by state
+        try: 
+            return self.matches[state]
+        except IndexError:
+            return None
