a7f6d8c6656eeb42bdd1ed566852efb020b321f2
t0x00 <jonathan.pepin@protonmail.com>
2019-01-14 17:42:37 -0500

    redis initiation on kali fix

--- core/utils.py
+++ core/utils.py
@@ -130,7 +130,7 @@ class Utils:
         if not os.getuid() == 0:
             UI.error('root privileges required to install the dependencies')
 
-        os.system('/usr/bin/apt update && /usr/bin/apt install redis-server mono-dmcs python-tabulate python-redis python-flask python-dev libxml2-dev libxslt-dev python-pip -y && pip install flask-socketIO && service redis-server start')
+        os.system('/usr/bin/apt update && /usr/bin/apt install redis-server mono-dmcs python-tabulate python-redis python-flask python-dev libxml2-dev libxslt-dev python-pip -y && pip install flask-socketIO && redis-server /etc/redis/redis.conf')
         UI.error('Installation completed please restart ThunderShell',True)
 
     @staticmethod
